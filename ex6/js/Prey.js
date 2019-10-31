058100d22e1571287b042dc50939287eddb1cb98
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2019-10-31 16:20:05 -0400

    ex6: all fixes
    problem commiting everytime. fixed spelling mistakes,missing variables, missing method and missing or extra brakets

---
+++ ex6/js/Prey.js
@@ -0,0 +1,102 @@
+// Prey
+//
+// A class that represents a simple prey that moves
+// on screen based on a noise() function. It can move around
+// the screen and be consumed by Predator objects.
+
+/////////////////////////
+// ~10 ERRORS IN HERE
+/////////////////////////
+
+class Prey { ///////class misspelled
+
+  // constructor
+  //
+  // Sets the initial values for the Predator's properties
+  // Either sets default values or uses the arguments provided
+  constructor(x, y, speed, fillColor, radius) {////// misspelled as why instead of y
+    // Position
+    this.x = x;
+    this.y = y;
+    // Velocity and speed
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = speed; ////speed misspelled
+    // Time properties for noise() function
+    this.tx = random(0, 200); // To make x and y noise different/////variables incorrect (it can't be between 0,0)
+    this.ty = random(0, 200); // we use random starting values/////variables incorrect (it can't be between 0,0)
+    // Health properties
+    this.maxHealth = radius;
+    this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
+    // Display properties
+    this.fillColor = fillColor;
+    this.radius = this.health;
+  }
+
+  // move
+  //
+  // Sets velocity based on the noise() function and the Prey's speed
+  // Moves based on the resulting velocity and handles wrapping
+  move() {/////move misspelled
+    // Set velocity via noise()
+    this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
+    this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
+    // Update position
+    this.x += this.vx;
+    this.y += this.vy;
+    // Update time properties
+    this.tx += 0.01;
+    this.ty += 0.01;
+    // Handle wrapping
+    this.handleWrapping();/////handlewrapping misspelled
+  }//////missing braket
+
+
+    // handleWrapping
+    //
+    // Checks if the prey has gone off the canvas and
+    // wraps it to the other side if so
+    handleWrapping() {
+      // Off the left or right
+      if (this.x < 0) {///// wrong sign, should be < instead of >
+        this.x += width;
+      }
+      else if (this.x > width) {
+        this.x -= width;
+      }
+      // Off the top or bottom
+      if (this.y < 0) {
+        this.y += height;
+      }
+      else if (this.y > height) {
+        this.y -= height;///////height misspelled
+      }
+    }
+
+    // display
+    //
+    // Draw the prey as an ellipse on the canvas
+    // with a radius the same size as its current health.
+    display() {
+      push();
+      noStroke();
+      fill(this.fillColor);
+      this.radius = this.health;
+      ellipse(this.x, this.y, this.radius * 2);/////written two instead of 2
+      pop();
+    }
+
+    // reset
+    //
+    // Set the position to a random location and reset health
+    // and radius back to default
+    reset() {
+      // Random position
+      this.x = random(0, width);
+      this.y = random(0, height);
+      // Default health
+      this.health = this.maxHealth;
+      // Default radius
+      this.radius = this.health;
+    }
+  }
---
+++ ex6/js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// Predator-Prey Simulation
+// by Naomi Ifergan
+//
+// Creates a predator and three prey (of different sizes and speeds)
+// The predator chases the prey using the arrow keys and consumes them.
+// The predator loses health over time, so must keep eating to survive.
+
+////////////////////
+// 10 ERRORS IN HERE
+////////////////////
+
+// Our predator
+let tiger;
+
+// The three prey
+let antelope;/////missing e at the end
+let zebra;
+let bee;
+
+// setup()
+//
+// Sets up a canvas
+// Creates objects for the predator and three prey
+function setup() { ////////// function setup was missing a space
+  createCanvas(windowWidth, windowHeight);
+  tiger = new Predator(100 , 100, 5, color(200, 200, 0), 40);/////extra comma
+  antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
+  zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);//////missing vraiable for y
+  bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
+}
+
+// draw()
+//
+// Handles input, movement, eating, and displaying for the system's objects
+function draw() {
+  // Clear the background to black
+  background(0);///backrgound was misspelled
+
+  // Handle input for the tiger
+  tiger.handleInput();//////missing method
+
+  // Move all the "animals"
+  tiger.move();
+  antelope.move();
+  zebra.move();
+  bee.move();////////// bee.move was missing
+  // Handle the tiger eating any of the prey
+  tiger.handleEating(antelope);
+  tiger.handleEating(zebra);
+  tiger.handleEating(bee);
+
+  // Display all the "animals"
+  tiger.display();
+  antelope.display();//////missing e
+  zebra.display();//////////display was misspelled
+  bee.display();////bee was misspelled
+}
