87c368251f579be32a7d47410a079d0a5b759d5a
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2019-11-21 11:28:16 -0500

    started ex 8
    1. Changed the title of the game.
    2. Added another player
    3. Changed the background image
    4. Used Arrays
    5. Added new instructions
    6. Changed the Instuction page and the gameover page.

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -1,88 +1,166 @@
-// "Catch Me If You Can"
-// by Naomi Ifergan
-//
-// This game is based on real life basketball. However, the player has 1 minute to score as much as he can before time runs out!
+      // "A minute to win it"
+      // by Naomi Ifergan
+      //
+      // This game is based on real life basketball. However, the players have 1 minute to score as much as he can before time runs out!
 
 
-//our background image
-let courtBackground;
+      //our background image
+      let courtBackground;
 
-// Our predator
-let nbaPlayer;
+      // player 1
+      let nbaPlayer;
 
-// The three prey
-let basketball;
+      // player 2
+      let player2;
 
-//arrays
-//the number of preys that will be simulated
-let prey=[];
-let numPrey = 25;
+      //basketballs
+      let basketball;
 
-function preload(){
-//placing background image for the game
-courtBackground = loadImage('assets/images/court.jpg');
+      //arrays
+      //number of preys that will be simulated
+      let prey = [];
+      let numPrey = 20;
 
-//placing nbaPlayer (predator)
-nbaPlayerImg = loadImage('assets/images/nbaPlayer.png');
+      // introduction page
+      let state = "TITLE";
 
-//placing basketball (prey)
-basketballImg = loadImage('assets/images/basketball.png');
-}
 
-// setup()
-//
-// Sets up a canvas
-// Creates objects for the predator and three prey
-function setup() {
-  createCanvas(1000,600);
-  //set up predator and prey
-  nbaPlayer = new Predator(100, 100, 5, 100,nbaPlayerImg);
-  basketball = new Prey(100, 100, 10, 50, basketballImg);
+  function preload(){
+      //placing background image for the game
+      courtBackground = loadImage('assets/images/court.jpg');
 
-  let preyImage = [basketballImg];
-    //Setting up prey
-   basketball = new Prey(100, 100, 10, 40,basketballImg);
+      //placing nbaPlayer
+      nbaPlayerImg = loadImage('assets/images/nbaPlayer.png');
 
-   //arrays
-   for (let i = 0; i < numPrey; i++) {
-     let preyX = random(0, width);
-     let preyY = random (0, height);
-     let preySpeed = random (2,10);
-     let preyRadius = random (3,50);
-     let randomIndex = floor (random(0,5));
-     let newPrey = new Prey (preyX, preyY, preySpeed, preyRadius, preyImage[randomIndex]);
+      //placing player 2
+      player2Img = loadImage ('assets/images/player2.png');
+
+      //placing basketballs
+      basketballImg = loadImage ('assets/images/basketball.png');
+
+      // setup()
+      //
+      // Sets up a canvas
+      // Creates objects for the players
+
+  function setup() {
+      createCanvas(1000,400);
+      //set up players
+      nbaPlayer = new Predator(100, 100, 5, 100,nbaPlayerImg);
+      player2 = new Predator (200,10,5,100,player2Img);
+
+      let preyImage = [basketballImg];
+      basketball = new prey (100,100,10,50, basketballImg);
+
+    //arrays
+  for (let i= 0; i, numPrey; i++){
+      let preyX = random(0, width);
+      let preyY = random (0, height);
+      let preySpeed = random (2,10);
+      let preyRadius = random (3,50);
+      let randomIndex = floor (random(0,5));
+      let newPrey = new Prey (preyX, preyY, preySpeed, preyRadius, preyImages[randomIndex]);
      prey.push(newPrey);
-   }
- }
+      }
+      }
+
+      // draw()
+      //
+      // Handles input, movement, eating, and displaying for the system's objects
+
+   function draw() {
+      // placing background image
+      imageMode(CENTER);
+      image(courtBackground,width/2,height/2, width,height);
+      }
+
+      //arrays
+    for (let i = 0; i< prey.length; i++) {
+      prey[i].move();
+      prey[i].display();
+      }
+
+      if (state === "TITLE") {
+       displayTitleScreen();
+       // We will imagine we have a function to show the title stuff
+     }
+
+      else if (state === "PLAY") {
 
 
-// draw()
-//
-// Handles input, movement, eating, and displaying for the system's objects
-function draw() {
-  // placing background image
-  imageMode(CENTER);
-image(courtBackground,width/2,height/2, width,height);
+      // Handle input for the Players
+      nbaPlayer.handleInput();
+      player2.handleInput();
+      //
+      // Move the players
+      nbaPlayer.move();
+      player2.move();
 
-//arrays
-for (let i = 0; i< prey.length; i++) {
-  prey[i].move();
-  prey[i].display();
-}
+      // Handle the player "eating" the prey (basketballs)
+      nbaPlayer.handleEating(basketball);
+      player2.handleEating (basketball);
 
-  // Handle input for the tiger
-  nbaPlayer.handleInput();
+      // Display the player and the basketballs
+      nbaPlayer.display();
+      player2.display();
 
-  // Move all the "animals"
-  nbaPlayer.move();
-  basketball.move();
+      //display the nbaPlayer's Score
+      displayScore();
+      if (nbaPlayer.health== 0) {
+      state= "GAMEOVER"
+      }
 
-  // Handle the tiger eating any of the prey
-  nbaPlayer.handleEating(basketball);
+      
+      }
+
+      else if (state === "GAMEOVER") {
+      displayGameOver();
+      }
+
+  function mousePressed() {
+      if (state === "GAMEOVER") {
+      // If we are on the gameover page, we want to play again
+      state = "PLAY";
+      }
+      }
+
+  function mousePressed() {
+        if (state === "TITLE") {
+        // If we were on the title we need to switch to instructions
+        state = "PLAY";
+        }
+        }
+
+    function displayScore (){
+      //display the score throughout the game
+      textAlign(CENTER,CENTER);
+      fill(0);
+      textSize(50);
+      textFont("Arial");
+      text("Score:" + (nbaPlayer.score),width/2,30);
+      }
+
+    function displayTitleScreen(){
+      //Title page
+      ellipseMode(CENTER);
+      fill(255,140,0); //orange
+      ellipse(width/2,height/2,300,300);
+      textSize(20);
+      textAlign(CENTER,CENTER);
+      fill(0);
+      text("Hey there basketball fans, \n Welcome to my court! \n You have 1 minute to beat your opponent. \n Player 1 use arrow keys to move. \n Player 2 use WASD keys to move. \n Click to play!\n ", width/2,height/2)
+      }
 
 
-  // Display all the "animals"
-  nbaPlayer.display();
-  basketball.display();
+    function displayGameOver(){
+      //Gameover page
+      ellipseMode(CENTER);
+      fill(204,0,0);
+      ellipse(width/2,height/2,300,300);
+      textSize(20);
+      textAlign(CENTER,CENTER);
+      fill(0);
+      text("GAMEOVER!", width/2,height/2)
 
-}
+      }
+      }
